509475b3532c7c0621fa5940a274ae928b35a466
dragon-rg <rigved.me@gmail.com>
2025-11-11 13:38:46 -0800

    binded controller button x to Drive.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -53,7 +53,7 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-    //m_driverController.x().whileTrue(m_Drive.execute());
+    m_driverController.x().whileTrue(m_Drive);
     
   }
 
